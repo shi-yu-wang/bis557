edb1147ac078b39d859e24d33bc95c3f127f116f
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-09-22 10:24:34 -0400

    Update linear-models.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -5,37 +5,41 @@
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
+#' @importFrom stats as.formula model.matrix terms
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  intercept<-as.data.frame(rep(1,nrow(data)))
-  # extract variables
-  var<-all.vars(formula)
-  # create design matrix
-  x<-model.matrix(formula, data = data)
-  y<-data[,var[1]]
-  # qr decomposition
-  xqr<-qr(x)
-  # calculate coefficients
-  coefficients<-as.vector(qr.coef(xqr,y))
-  fit_model<-list()
-  fit_model$coefficients<-coefficients
-  fit_model$residuals<-NA
-  fit_model$fitted.values<-as.vector(x %*% as.matrix(fit_model$coefficients))
-  fit_model$rank<-NA
-  fit_model$weights<-NA
-  fit_model$df.residual<-NA
-  fit_model$call<-NA
-  fit_model$terms<-NA
-  fit_model$contrasts<-NA
-  fit_model$xlevels<-NA
-  fit_model$offset<-NA
-  fit_model$y<-NA
-  fit_model$x<-NA
-  fit_model$model<-NA
-  fit_model$na.action<-NA
-  class(fit_model)<-"lm"
-  return(fit_model)
+  f = all.vars(formula)
+  
+  ## Get the response variable and the design matrix
+  X = model.matrix(formula, data = data)
+  Y = data[, f[1]]
+  
+  ## Create a list to store all components of an object class of "lm"
+  fit_linear_model = list()
+  
+  ## All components of an object class of "lm"
+  fit_linear_model$coefficients = qr.coef(qr(X), Y)
+  fit_linear_model$fitted.values = X %*% fit_linear_model$coefficients
+  fit_linear_model$residuals = fit_linear_model$y - fit_linear_model$fitted.values
+  fit_linear_model$rank = ncol(X)
+  fit_linear_model$weights = NULL
+  fit_linear_model$df.residual = nrow(X) - ncol(X)
+  fit_linear_model$call = call("linear_model", formula)
+  fit_linear_model$terms = terms(x = formula, data = data)
+  fit_linear_model$contrasts = NULL
+  fit_linear_model$xlevels = NULL
+  fit_linear_model$offset = NULL
+  fit_linear_model$x = X
+  fit_linear_model$y = Y
+  fit_linear_model$model = formula
+  fit_linear_model$qr = qr(X)
+  
+  ## Set the list as an object class of "lm"
+  class(fit_linear_model) = "lm"
+  
+  ## Return the list
+  return(fit_linear_model)
 }

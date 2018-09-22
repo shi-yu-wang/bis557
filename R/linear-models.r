0b6c7877823e9e80dfdfa22b0bd863def393176d
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-09-22 10:56:59 -0400

    Update linear-models.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -5,7 +5,6 @@
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
-#' @importFrom stats as.formula model.matrix terms
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
@@ -22,16 +21,16 @@ linear_model <- function(formula, data) {
   fit_model<-list()
   # calculate coefficients
   fit_model$coefficients<-qr.coef(xqr,y)
-  fit_model$fitted.values<-NA
-  fit_model$residuals<-NA
-  fit_model$rank<-NA
-  fit_model$weights<-NA
-  fit_model$df.residual<-NA
+  fit_model$fitted.values<-NULL
+  fit_model$residuals<-NULL
+  fit_model$rank<-NULL
+  fit_model$weights<-NULL
+  fit_model$df.residual<-NULL
   fit_model$call<-call("linear_model",formula)
-  fit_model$terms<-NA
-  fit_model$contrasts<-NA
-  fit_model$xlevels<-NA
-  fit_model$offset<-NA
+  fit_model$terms<-NULL
+  fit_model$contrasts<-NULL
+  fit_model$xlevels<-NULL
+  fit_model$offset<-NULL
   fit_model$x<-x
   fit_model$y<-y
   fit_model$model<-formula

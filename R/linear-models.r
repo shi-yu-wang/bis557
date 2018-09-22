6e0d136023a728f80fcbe763eaafe1bb4cbc7a81
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-09-22 11:55:45 -0400

    Update linear-models.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -10,31 +10,96 @@
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  # extract variables
-  var<-all.vars(formula)
-  # create design matrix
-  x<-model.matrix(formula, data = data)
-  y<-data[,var[1]]
-  # qr decomposition
-  xqr<-qr(x)
-  # list to contain lm items
-  fit_model<-list()
+  intercept<-as.data.frame(rep(1,nrow(data)))
+  formula_check<-as.character(formula)
+  if ("." %in% formula_check){
+    formula_check<-formula_check
+  } else{
+    tm<-terms(formula)
+    var<-as.character(attr(tm, "variables"))[-1]
+  }
+  # generate dataset for analysis
+  if ("." %in% formula_check){
+    data<-data
+  } else {
+  data<-data[,match(var,colnames(data))]
+  }
+  # If factor variable exists, change factor variables to numeric variables
+  colno<-c()
+  for (i in 1:ncol(data)) {
+    if (class(data[,i])!="numeric" & class(data[,i])!="integer"){
+      data[,i]<-as.factor(data[,i])
+      colno<-c(colno,i)
+      f<-levels(data[,i])
+      data[,i]<-as.character(data[,i])
+      for (j in 2:length(f)) {
+        v<-data[,i]
+        v[v==f[j]]<-1
+        v[v!="1"]<-0
+        v<-as.numeric(v)
+        v<-as.data.frame(v)
+        colnames(v)<-paste(colnames(data)[i], f[j],sep = "")
+        data<-cbind(data,v)
+      }
+    }
+  }
+  if (length(colno)>0){
+    data<-data[,-colno]
+  } else {
+    data<-data
+  }
   # calculate coefficients
-  fit_model$coefficients<-qr.coef(xqr,y)
+  if ("." %in% formula_check){
+    y<-as.matrix((data[,match(formula_check[2],colnames(data))]))
+    x<-as.data.frame(data[,-match(formula_check[2],colnames(data))])
+    xvar<-colnames(x)
+    x<-as.matrix(cbind(intercept,x))
+    xqr<-qr(x)
+    coefficients<-qr.coef(xqr,y)
+    fit_model<-list()
+    fit_model$coefficients<-coefficients
+    fit_model$fitted.values<-x %*% fit_model$coefficients
+    fit_model$residuals<-fit_model$fitted.values-fit_model$y
+    fit_model$rank<-NULL
+    fit_model$weights<-NULL
+    fit_model$df.residual<-NULL
+    fit_model$call<-NULL
+    fit_model$terms<-NULL
+    fit_model$contrasts<-NULL
+    fit_model$xlevels<-NULL
+    fit_model$offset<-NULL
+    fit_model$y<-y
+    fit_model$x<-y
+    fit_model$model<-NULL
+    fit_model$na.action<-NULL
+    class(fit_model)<-"lm"
+    return(fit_model)
+  } else {
+  resp<-attr(tm, "response")
+  yvar<-var[resp]
+  y<-as.matrix((data[,match(yvar,colnames(data))]))
+  x<-as.data.frame(data[,-match(yvar,colnames(data))])
+  xvar<-colnames(x)
+  x<-as.matrix(cbind(intercept,x))
+  xqr<-qr(x)
+  coefficients<-qr.coef(xqr,y)
+  fit_model<-list()
+  fit_model$coefficients<-coefficients
   fit_model$fitted.values<-x %*% fit_model$coefficients
-  fit_model$residuals<-fit_model$y - fit_model$fitted.values
-  fit_model$rank<-col(x)
+  fit_model$residuals<-fit_model$fitted.values-fit_model$y
+  fit_model$rank<-NULL
   fit_model$weights<-NULL
-  fit_model$df.residual<-nrow(x)-ncol(x)
-  fit_model$call<-call("linear_model", formula)
-  fit_model$terms<-terms(x = formula, data = data)
+  fit_model$df.residual<-NULL
+  fit_model$call<-NULL
+  fit_model$terms<-NULL
   fit_model$contrasts<-NULL
   fit_model$xlevels<-NULL
   fit_model$offset<-NULL
-  fit_model$x<-x
   fit_model$y<-y
-  fit_model$model<-formula
-  fit_model$qr<-qr(x)
+  fit_model$x<-x
+  fit_model$model<-NULL
+  fit_model$na.action<-NULL
   class(fit_model)<-"lm"
   return(fit_model)
+  }
 }

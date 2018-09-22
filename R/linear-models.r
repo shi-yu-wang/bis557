6c3dd55dc8e6342d65c8af22a202ec498d45216a
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-09-22 11:01:53 -0400

    Update linear-models.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -11,30 +11,30 @@
 #' @export
 linear_model <- function(formula, data) {
   # extract variables
-  var<-all.vars(formula)
+  var=all.vars(formula)
   # create design matrix
-  x<-model.matrix(formula, data = data)
-  y<-data[,var[1]]
+  x=model.matrix(formula, data = data)
+  y=data[,var[1]]
   # qr decomposition
-  xqr<-qr(x)
+  xqr=qr(x)
   # list to contain lm items
-  fit_model<-list()
+  fit_model=list()
   # calculate coefficients
-  fit_model$coefficients<-qr.coef(xqr,y)
-  fit_model$fitted.values<-NULL
-  fit_model$residuals<-NULL
-  fit_model$rank<-NULL
-  fit_model$weights<-NULL
-  fit_model$df.residual<-NULL
-  fit_model$call<-call("linear_model",formula)
-  fit_model$terms<-NULL
-  fit_model$contrasts<-NULL
-  fit_model$xlevels<-NULL
-  fit_model$offset<-NULL
-  fit_model$x<-x
-  fit_model$y<-y
-  fit_model$model<-formula
-  fit_model$qr<-qr(x)
-  class(fit_model)<-"lm"
+  fit_model$coefficients=qr.coef(xqr,y)
+  fit_model$fitted.values=NULL
+  fit_model$residuals=NULL
+  fit_model$rank=NULL
+  fit_model$weights=NULL
+  fit_model$df.residual=NULL
+  fit_model$call=call("linear_model",formula)
+  fit_model$terms=NULL
+  fit_model$contrasts=NULL
+  fit_model$xlevels=NULL
+  fit_model$offset=NULL
+  fit_model$x=x
+  fit_model$y=y
+  fit_model$model=formula
+  fit_model$qr=qr(x)
+  class(fit_model)="lm"
   return(fit_model)
 }

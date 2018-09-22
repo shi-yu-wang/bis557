434662e866ebfa0b288807a96d82b48438fc7a12
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-09-22 11:31:06 -0400

    Update linear-models.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -24,13 +24,13 @@ linear_model <- function(formula, data) {
   fit_model$fitted.values<-x %*% fit_model$coefficients
   fit_model$residuals<-fit_model$y - fit_model$fitted.values
   fit_model$rank<-col(x)
-  fit_model$weights<-NA
+  fit_model$weights<-NULL
   fit_model$df.residual<-nrow(x)-ncol(x)
   fit_model$call<-call("linear_model", formula)
   fit_model$terms<-terms(x = formula, data = data)
-  fit_model$contrasts<-NA
-  fit_model$xlevels<-NA
-  fit_model$offset<-NA
+  fit_model$contrasts<-NULL
+  fit_model$xlevels<-NULL
+  fit_model$offset<-NULL
   fit_model$x<-x
   fit_model$y<-y
   fit_model$model<-formula

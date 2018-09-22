b5f876a139c16b7489d1dfef7a942f408a6b379e
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-09-22 00:29:16 -0400

    Update linear-models.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -10,5 +10,39 @@
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  lm(formula, data)
+  intercept<-as.data.frame(rep(1,nrow(data)))
+  formula_check<-as.character(formula)
+  # when all variable in dataset are included
+  if ("." %in% formula_check){
+    y<-as.matrix((data[,match(formula_check[2],colnames(data))]))
+    x<-as.data.frame(data[,-match(formula_check[2],colnames(data))])
+    xvar<-colnames(x)
+    x<-as.matrix(cbind(intercept,x))
+    svd_output <- svd(x)
+    U <- svd_output[["u"]]
+    D <- 1 / svd_output[["d"]]
+    Sinv <- diag(D,nrow = length(D), ncol = length(D))
+    V <- svd_output[["v"]]
+    pseudo_inv <- V %*% Sinv %*% t(U)
+    coefficients <- as.vector(pseudo_inv %*% y)
+    names(coefficients)<-c("(Intercept)",xvar)
+    linear_model<-list(coefficients=coefficients)
+  } else {
+  tm<-terms(formula)
+  var<-as.character(attr(tm, "variables"))[-1]
+  resp<-attr(tm, "response")
+  yvar<-var[resp]
+  xvar<-var[-resp]
+  y<-as.matrix((data[,match(yvar,colnames(data))]))
+  x<-as.data.frame(data[,match(xvar,colnames(data))])
+  x<-as.matrix(cbind(intercept,x))
+  svd_output <- svd(x)
+  U <- svd_output[["u"]]
+  D <- 1 / svd_output[["d"]]
+  Sinv <- diag(D,nrow = length(D), ncol = length(D))
+  V <- svd_output[["v"]]
+  pseudo_inv <- V %*% Sinv %*% t(U)
+  coefficients <- as.vector(pseudo_inv %*% y)
+  names(coefficients)<-c("(Intercept)",xvar)
+  linear_model<-list(coefficients=coefficients)}
 }

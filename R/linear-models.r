98de13f52aec94e75ca735544001a5ebece7eab4
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-09-22 02:30:59 -0400

    Update linear-models.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -12,10 +12,22 @@
 linear_model <- function(formula, data) {
   intercept<-as.data.frame(rep(1,nrow(data)))
   formula_check<-as.character(formula)
+  if ("." %in% formula_check){
+    formula_check<-formula_check
+  } else{
+    tm<-terms(formula)
+    var<-as.character(attr(tm, "variables"))[-1]
+  }
+  # generate dataset for analysis
+  if ("." %in% formula_check){
+    data<-data
+  } else {
+  data<-data[,match(var,colnames(data))]
+  }
   # If factor variable exists, change factor variables to numeric variables
   colno<-c()
   for (i in 1:ncol(data)) {
-    if (class(data[,i]) != "numeric"){
+    if (class(data[,i])!="numeric" & class(data[,i])!="integer"){
       data[,i]<-as.factor(data[,i])
       colno<-c(colno,i)
       f<-levels(data[,i])
@@ -31,40 +43,29 @@ linear_model <- function(formula, data) {
       }
     }
   }
-  # remove original factor variables
   if (length(colno>0)){
     data<-data[,-colno]
   }
+  # calculate coefficients
   if ("." %in% formula_check){
     y<-as.matrix((data[,match(formula_check[2],colnames(data))]))
     x<-as.data.frame(data[,-match(formula_check[2],colnames(data))])
     xvar<-colnames(x)
     x<-as.matrix(cbind(intercept,x))
-    svd_output <- svd(x)
-    U <- svd_output[["u"]]
-    D <- 1 / svd_output[["d"]]
-    Sinv <- diag(D,nrow = length(D), ncol = length(D))
-    V <- svd_output[["v"]]
-    pseudo_inv <- V %*% Sinv %*% t(U)
-    coefficients <- as.vector(pseudo_inv %*% y)
+    xqr<-qr(x)
+    coefficients<-as.vector(qr.coef(xqr,y))
     names(coefficients)<-c("(Intercept)",xvar)
     linear_model<-list(coefficients=coefficients)
   } else {
-  tm<-terms(formula)
-  var<-as.character(attr(tm, "variables"))[-1]
   resp<-attr(tm, "response")
   yvar<-var[resp]
-  xvar<-var[-resp]
   y<-as.matrix((data[,match(yvar,colnames(data))]))
-  x<-as.data.frame(data[,match(xvar,colnames(data))])
+  x<-as.data.frame(data[,-match(yvar,colnames(data))])
+  xvar<-colnames(x)
   x<-as.matrix(cbind(intercept,x))
-  svd_output <- svd(x)
-  U <- svd_output[["u"]]
-  D <- 1 / svd_output[["d"]]
-  Sinv <- diag(D,nrow = length(D), ncol = length(D))
-  V <- svd_output[["v"]]
-  pseudo_inv <- V %*% Sinv %*% t(U)
-  coefficients <- as.vector(pseudo_inv %*% y)
+  xqr<-qr(x)
+  coefficients<-as.vector(qr.coef(xqr,y))
   names(coefficients)<-c("(Intercept)",xvar)
-  linear_model<-list(coefficients=coefficients)}
+  linear_model<-list(coefficients=coefficients)
+  }
 }

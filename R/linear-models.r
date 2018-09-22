62e8f56837f53a2989b7d697da4337f34b7cb316
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-09-22 03:41:25 -0400

    Update linear-models.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -57,6 +57,20 @@ linear_model <- function(formula, data) {
     names(coefficients)<-c("(Intercept)",xvar)
     fit_model<-list()
     fit_model$coefficients<-coefficients
+    fit_model$residuals<-NA
+    fit_model$fitted.values<-NA
+    fit_model$rank<-NA
+    fit_model$weights<-NA
+    fit_model$df.residual<-NA
+    fit_model$call<-NA
+    fit_model$terms<-NA
+    fit_model$contrasts<-NA
+    fit_model$xlevels<-NA
+    fit_model$offset<-NA
+    fit_model$y<-NA
+    fit_model$x<-NA
+    fit_model$model<-NA
+    fit_model$na.action<-NA
     class(fit_model)<-"lm"
   } else {
   resp<-attr(tm, "response")
@@ -70,6 +84,20 @@ linear_model <- function(formula, data) {
   names(coefficients)<-c("(Intercept)",xvar)
   fit_model<-list()
   fit_model$coefficients<-coefficients
+  fit_model$residuals<-NA
+  fit_model$fitted.values<-NA
+  fit_model$rank<-NA
+  fit_model$weights<-NA
+  fit_model$df.residual<-NA
+  fit_model$call<-NA
+  fit_model$terms<-NA
+  fit_model$contrasts<-NA
+  fit_model$xlevels<-NA
+  fit_model$offset<-NA
+  fit_model$y<-NA
+  fit_model$x<-NA
+  fit_model$model<-NA
+  fit_model$na.action<-NA
   class(fit_model)<-"lm"
   }
   return(fit_model)

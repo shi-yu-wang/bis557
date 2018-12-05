88124a2ceb92caecbbd84474fd1f4054536293e9
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-12-05 18:08:33 -0500

    Update sparse_matrix.R

--- R/sparse_matrix.R
+++ R/sparse_matrix.R
@@ -33,7 +33,7 @@ sparse.matrix <- function(i, j, x, dims = c(max(i), max(j))){
 }
 
 # add
-`+` <- function(a, b){
+`+.sparse.matrix` <- function(a, b){
   if (!identical(a[[2]], b[[2]]))
     stop("dimensions not match")
   c <- merge(a[[1]], b[[1]], by = c("i", "j"), all = TRUE, suffixes = c("1", "2"))
@@ -54,7 +54,7 @@ sparse.matrix <- function(i, j, x, dims = c(max(i), max(j))){
   UseMethod("%*%", x)
 }
 
-`%*%` <- function(a, b){
+`%*%.sparse.matrix` <- function(a, b){
   if ((a[[2]][2] != b[[2]][1]))
     stop("dimensions not match")
   colnames(b[[1]]) <- c("i2", "j2", "x2")
@@ -75,7 +75,7 @@ t <- function (x, ...) {
   UseMethod("t", x)
 }
 
-`t` <- function(a){
+`t.sparse.matrix` <- function(a){
   temp <- a[[1]]$i
   a[[1]]$i <- a[[1]]$j
   a[[1]]$j <- temp

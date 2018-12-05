a128259a4fb3826cff2c8cc774f5ca88f2eb10f4
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-12-05 16:22:58 -0500

    Add files via upload

---
+++ R/sparse_matrix.R
@@ -0,0 +1,89 @@
+#' @description generate sparse matrix using sparse.matrix, using add, multiply and transpose
+#' @param i row indeces of non-zero elements
+#' @param j column indeces of non-zero elements
+#' @param x non-zero values
+#' @param dims the dimension of generated sparse matrix
+#' @return a sparse.matrix
+#' @export
+
+# 3
+### Note: get hints from Wenfeng Zhang for this problem.
+sparse_add <- function(a, b) {
+  c <- merge(a, b, by = c("i", "j"), all = TRUE, suffixes = c("1", "2"))
+  c$x1[is.na(c$x1)] <- 0
+  c$x2[is.na(c$x2)] <- 0
+  c$x <- c$x1 + c$x2
+  c[, c("i", "j", "x")]
+}
+a <- data.frame(i = c(1, 2), j = c(1, 1), x = c(3, 1))
+b <- data.frame(i = c(1, 2, 2), j = c(1, 1, 2), x = c(4.4, 1.2, 3))
+sparse_add(a, b)
+
+# Implement a `sparse_multiply` function that multiplies two sparse matrices.
+casl_sparse_multiply <-
+  function(a, b){
+    colnames(b) <- c("i2", "j2", "x2")
+    c <- merge(a, b, by.x = "j", by.y = "i2",
+               all = FALSE, suffixes = c("1", "2"))
+    c$x <- c$x * c$x2
+    c$key <- paste(c$i, c$j, sep = "-")
+    x <- tapply(c$x, c$key, sum)
+    key <- strsplit(names(x), "-")
+    d <- data.frame(i = sapply(key, getElement, 1),
+                    j = sapply(key, getElement, 2),
+                    x = as.numeric(x))
+    d
+  }
+a <- data.frame(i = c(1, 2), j = c(1, 1), x = c(3, 1))
+b <- data.frame(i = c(1, 2, 2), j = c(1, 1, 2), x = c(4.4, 1.2, 3))
+casl_sparse_multiply(a, b)
+
+# Define class
+sparse.matrix <- function(i, j, x, dims = c(max(i), max(j))){
+  structure(list(data.frame(i = c(1, 2), j = c(1, 1), x = c(3, 1)), dims), class = "sparse.matrix")
+}
+
+# Add
+`+`<-function(a, b){
+  c <- merge(a[[1]], b[[1]], by = c("i", "j"), all = TRUE, suffixes = c("1", "2"))
+  c$x1[is.na(c$x1)] <- 0
+  c$x2[is.na(c$x2)] <- 0
+  c$x <- c$x1 + c$x2
+  c[, c("i", "j", "x")]
+  sparse.matrix(c$i, c$j, c$x, dims = a[[2]])
+}
+
+# Multiply
+`%*%` <- function(a, b){
+  if ((a[[2]][2] != b[[2]][1]))
+    stop("dimensions not match")
+  colnames(b[[1]]) <- c("i2", "j2", "x2")
+  c <- merge(a[[1]], b[[1]], by.x = "j", by.y = "i2",
+             all = FALSE, suffixes = c("1", "2"))
+  c$x <- c$x * c$x2
+  c$key <- paste(c$i, c$j, sep = "-")
+  x <- tapply(c$x, c$key, sum)
+  key <- strsplit(names(x), "-")
+  d <- data.frame(i = sapply(key, getElement, 1),
+                  j = sapply(key, getElement, 2),
+                  x = as.numeric(x))
+  sparse.matrix(c$i, c$j, c$x, dims = c(a[[2]][1], b[[2]][2]))
+}
+
+# Transpose
+t <- function (x, ...) {
+  UseMethod("t", x)
+}
+
+`t` <- function(a){
+  temp <- a[[1]]$i
+  a[[1]]$i <- a[[1]]$j
+  a[[1]]$j <- temp
+  a[[2]] <- rev(a[[2]])
+  return(a)
+}
+
+
+
+
+

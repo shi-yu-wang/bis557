a51ac7077e2df20e33febb948baffcd79beb90b3
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-12-05 19:30:25 -0500

    Update sparse_matrix.R

--- R/sparse_matrix.R
+++ R/sparse_matrix.R
@@ -62,18 +62,19 @@ stop("Incorrect dimensions")
 #' @export
 # The function
 sparse_matrix_multiply<- function(a, b){
-if((a[[2]][2]!=b[[2]][1]))
-stop("Incorrect dimensions")
-  colnames(b[[1]])<-c("i2", "j2", "x2")
-  c<-merge(a[[1]],b[[1]],by.x ="j",by.y="i2",all = FALSE,suffixes=c("1", "2"))
+  if ((a[[2]][2] != b[[2]][1]))
+  stop("dimensions not match")
+  colnames(b[[1]]) <- c("i2", "j2", "x2")
+  c<-merge(a[[1]], b[[1]], by.x = "j", by.y = "i2",
+  all = FALSE, suffixes = c("1", "2"))
   c$x<-c$x*c$x2
-  c$key <- paste(c$i, c$j, sep = "-")
-  x <- tapply(c$x, c$key, sum)
-  key <- strsplit(names(x), "-")
-  d<-data.frame(i = sapply(key, getElement, 1),j = sapply(key, getElement, 2),x = as.numeric(x))
-  spar_mtx <-list(mat=d,dims=c(a$dims[1], b$dims[2]))
-  class(spar_mtx) <- "sparse.matrix"
-  spar_mtx
+  c$key<-paste(c$i, c$j, sep = "-")
+  x<-tapply(c$x, c$key, sum)
+  key<-strsplit(names(x), "-")
+  d<-data.frame(i = sapply(key, getElement, 1),
+                  j = sapply(key, getElement, 2),
+                  x = as.numeric(x))
+  sparse.matrix(c$i, c$j, c$x, dims = c(a[[2]][1], b[[2]][2]))
 }
 `%*%.default` <- .Primitive("%*%")
 `%*%` <- function(x, y) {

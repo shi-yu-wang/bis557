f6357cd544a16bb20417312f098c78a4bfd8776e
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2018-12-05 20:06:04 -0500

    Update sparse_matrix.R

--- R/sparse_matrix.R
+++ R/sparse_matrix.R
@@ -42,14 +42,7 @@ stop("Incorrect dimensions")
 #' @return a sparse.matrix
 #' @export
 # The function
-`%*%.default` = .Primitive("%*%")  # keep defalut
-`%*%` = function(x,...){ 
-  UseMethod("%*%",x)
-}
-`%*%` <- function(x, y) {
-  UseMethod("%*%", x)
-}
-`%*%.sparse.matrix` <- function(a, b){
+sparse_matrix_multiply<- function(a, b){
   if ((a[[2]][2] != b[[2]][1]))
     stop("dimensions not match")
   colnames(b[[1]]) <- c("i2", "j2", "x2")
@@ -62,8 +55,20 @@ stop("Incorrect dimensions")
   d <- data.frame(i = sapply(key, getElement, 1),
                   j = sapply(key, getElement, 2),
                   x = as.numeric(x))
-  sparse.matrix(c$i, c$j, c$x, dims = c(a[[2]][1], b[[2]][2]))
+  d$i <- as.numeric(d$i)
+  d$j <- as.numeric(d$j)
+  spar_mtx <- list(mat = d, dims = c(a$dims[1], b$dims[2])) 
+  class(spar_mtx) <- "sparse.matrix" 
+  spar_mtx
 }
+# Implement
+`%*%.default` = .Primitive("%*%")
+`%*%` <- function(x, y) {
+  UseMethod("%*%", x)
+}
+`%*%.sparse.matrix` <-function(x,y){
+  sparse_matrix_multiply(x,y)
+  }
 # Transpose
 #' @description transposing a sparse matrix
 #' @param a a list for a sparse matrix
